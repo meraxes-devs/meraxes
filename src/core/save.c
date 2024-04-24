7d1d67893fbe9da027e0d0adaf568d85f1bba62a
ManuGT <eventura@student.unimelb.edu.au>
2024-04-24 15:31:34 +1000

    Wrong N of Prop

--- src/core/save.c
+++ src/core/save.c
@@ -136,7 +136,7 @@ void calc_hdf5_props()
     galaxy_output_t galout;
     int i; // dummy
 
-    h5props->n_props = 9;
+    h5props->n_props = 10;
 #if USE_SCALING_REL
     h5props->n_props += 2;
 #endif 
